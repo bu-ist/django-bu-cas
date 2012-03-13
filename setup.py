cf4f317482c9e0f6fc69117876c62c8d0ca1a4c5
Mike Burns <mgburns@bu.edu>
2012-03-13 17:34:49 -0400

    First attempt at packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+	name='django-bu-cas',
+	version='0.1.0',
+	author='Sergey Tetelbaum',
+	author_email='stetelba@bu.edu',
+	packages=['django_bucas',],
+	url='http://github.com/bu-ist/django-bu-cas',
+	license='LICENSE.txt',
+	description='CAS Authentication middleware for the BU Django environment',
+	long_description=open('README').read(),
+	install_requires=[
+		"Django >= 1.1.1",
+	],
+)
