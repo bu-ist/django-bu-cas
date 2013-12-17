82bf09df7784aab6c3bef8e95139bc253ea67b33
Fyodor Wolf <fwolf@bu.edu>
2013-12-17 11:37:19 -0500

    updated setup params.

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 
 setup(
 	name='django-bu-cas',
-	version='0.1.0',
-	author='Sergey Tetelbaum',
-	author_email='stetelba@bu.edu',
+	version='1.2.0',
+	author='Fyodor Wolf',
+	author_email='fwolf@bu.edu',
 	packages=['django_bucas',],
 	url='http://github.com/bu-ist/django-bu-cas',
 	license='LICENSE.txt',
 	description='CAS Authentication middleware for the BU Django environment',
-	long_description=open('README').read(),
+	long_description=open('README.md').read(),
 	install_requires=[
-		"Django >= 1.1.1",
+		"Django >= 1.5",
 	],
 )
